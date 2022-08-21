273d7a846fb1e033959b4f4d95c3bb474e1c60dc
TakanariShimbo <takanari.shimbo+github@gmail.com>
2022-08-22 02:40:15 +0900

    revise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import glob
 import cv2
 import numpy as np
+import tqdm
+
 
 # setting
 filename = "test_1"
@@ -21,26 +24,30 @@ params.minInertiaRatio = 0.08
 detector = cv2.SimpleBlobDetector_create(params)
 
 
-# main
-for i in range(1000):
-    image_path = '../datas/original_images/{}/image{}.bmp'.format(filename, i * 4)
-    mask2_path = '../datas/mask2_images/{}/mask{}.bmp'.format(filename, i*4)
+# read directory
+image_folder_path = f"../datas/original_images/{filename}/"
+mask2_folder_path = f"../datas/mask2_images/{filename}/"
+total_frame = len(glob.glob(image_folder_path+'*.bmp'))
 
-    img_bgr = cv2.imread(image_path)
+for i in tqdm.tqdm(range(total_frame)):
+    # read images
+    image_path = image_folder_path + f"frame{i}.bmp"
+    mask2_path = mask2_folder_path + f"frame{i}.bmp"
     img_mask2_gray = cv2.imread(mask2_path, 0)
-    img_mask2_bgr = img_bgr.copy()
-    img_mask2_bgr[img_mask2_gray == 0] = 0
-    image_height, image_width = img_mask2_gray.shape
+    img_bgr = cv2.imread(image_path)
+
+    # detect ball's key points
     key_points = detector.detect(img_mask2_gray)
 
-    # 一番白っぽいところをボールの位置とする
+    image_height, image_width = img_mask2_gray.shape
     if len(key_points) > 0:
         maxval = 0
         for i in range(len(key_points)):
+            # chose most white point as ball position
             x = int(key_points[i].pt[0])
             y = int(key_points[i].pt[1])
             val = np.sum(
-                img_mask2_bgr[
+                img_bgr[
                 max([y - 3, 0]): min([y + 3, image_height - 1]),
                 max([x - 3, 0]): min([x + 3, image_width - 1]),
                 ]
@@ -56,6 +63,9 @@ for i in range(1000):
         key_point = []
     pos = np.array([col, row])
 
+    # viz
+    img_mask2_bgr = img_bgr.copy()
+    img_mask2_bgr[img_mask2_gray == 0] = 0
     img_viz = cv2.addWeighted(img_mask2_bgr, 0.7, img_bgr, 0.3, 1.0)
     img_viz = cv2.drawKeypoints(img_viz, key_point, np.array([]), (0, 255, 0),
                               cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
--- make_mask2.py
+++ make_mask2.py
@@ -1,39 +1,52 @@
 import os
+import glob
 import cv2
+import tqdm
 import numpy as np
 import pandas as pd
 
 # setting
 filename = 'test_1'
 
-# make directory for save
-mask2_folder_path = "../datas/mask2_images/{}/".format(filename)
-os.makedirs(mask2_folder_path, exist_ok=True)
+# save directory
+save_folder_path = f"../datas/mask2_images/{filename}/"
+os.makedirs(save_folder_path, exist_ok=True)
 
-for i in range(1000):
-    mask_path = '../datas/mask_images/{}/mask{}.bmp'.format(filename, i*4)
-    image_path = '../datas/original_images/{}/image{}.bmp'.format(filename, i * 4)
-    positions_path = '../datas/humans_positions/{}/image{}.txt'.format(filename, i*4)
+# read directory
+image_folder_path = f"../datas/original_images/{filename}/"
+mask_folder_path = f"../datas/mask_images/{filename}/"
+humans_bbox_folder_path = f"../datas/humans_bbox/{filename}/labels/"
+total_frame = len(glob.glob(image_folder_path+'*.bmp'))
 
+for i in tqdm.tqdm(range(total_frame)):
+    # read images
+    image_path = image_folder_path + f"frame{i}.bmp"
+    mask_path = mask_folder_path + f"frame{i}.bmp"
     img_mask_gray = cv2.imread(mask_path, 0)
     img_bgr = cv2.imread(image_path)
-    positions_dataframe = pd.read_csv(positions_path, delimiter=' ', header=None)
+
+    # read yolo bbox
+    humans_bbox_path = humans_bbox_folder_path + f"frame{i}.txt"
+    humans_bbox_dataframe = pd.read_csv(humans_bbox_path, delimiter=' ', header=None)
 
     image_height, image_width = img_mask_gray.shape
-    for position_series in positions_dataframe.values:
+    for position_series in humans_bbox_dataframe.values:
+        # convert bbox yolo -> cv2
         x = position_series[1] * image_width
         y = position_series[2] * image_height
         w = position_series[3] * image_width * 1.2
         h = position_series[4] * image_height * 1.2
-
         p1 = np.array([x - w / 2, y - h / 2], dtype=int)
         p2 = np.array([x + w / 2, y + h / 2], dtype=int)
+
+        # mask bbox
         cv2.rectangle(img_mask_gray, p1, p2, 0, cv2.FILLED)
 
+    # viz
     img_mask2_bgr = img_bgr.copy()
     img_mask2_bgr[img_mask_gray == 0] = 0
     img_viz = cv2.addWeighted(img_mask2_bgr, 0.7, img_bgr, 0.3, 1.0)
     cv2.imshow("", img_viz)
     cv2.waitKey(1)
 
-    cv2.imwrite(mask2_folder_path + "mask{}.bmp".format(i*4), img_mask_gray)
+    cv2.imwrite(save_folder_path + "frame{}.bmp".format(i), img_mask_gray)
